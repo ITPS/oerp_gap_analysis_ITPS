820a07fe9adeced24e1523e679b8bb1c03e94911
Carlos Paredes <cparedes@covetel.com.ve>
2013-12-12 16:15:34 -0430

    Mejorando el código del script que añade todas as funcionalidades a un GAP
    Signed-off-by: Carlos Paredes <cparedes@covetel.com.ve>

--- utils/odf2gap/add_all_func_to_gap.py
+++ utils/odf2gap/add_all_func_to_gap.py
@@ -26,29 +26,27 @@ def actualizar(sock, uid):
     for i in cod:
         func = sock.execute(dbname, uid, pwd, 'gap_analysis', 'read', i, fields)
 
-        values = {'attr': 'value', 'attr' : 'value'}
+        values = {'filt': 'value', 'filt' : 'value'}
         
         modificar(sock, uid, i, values)
 
 def modificar(sock, uid, ids, values):
     account_id = sock.execute(dbname, uid, pwd, 'gap_analysis', 'write', ids, values)
 
-def buscar(sock, uid, code):
-    if code == "":
-        attr = []
-    else:
-        attr = code 
-    cod = sock.execute(dbname, uid, pwd, 'gap_analysis', 'search', attr)
+def buscar(sock, uid, filt):
+    if filt == "":
+        filt = []
+    cod = sock.execute(dbname, uid, pwd, 'gap_analysis', 'search', filt)
     return cod
 
-def listar(sock, uid, code):
+def listar(sock, uid, value):
     gaps = []
     fields = []
-    if code == "":
-        attr = []
+    if value == "":
+        filt = []
     else:
-        attr = [('name','=', code)]
-    cod = buscar(sock, uid, attr) 
+        filt = [('name','=', value)]
+    cod = buscar(sock, uid, filt)
     for i in cod:
         gap = sock.execute(dbname, uid, pwd, 'gap_analysis', 'read', i, fields)
         gaps.append(gap)
@@ -85,22 +83,20 @@ def crear_gap_lines(sock, uid, gap_name):
         id_gap_line = sock.execute(dbname, uid, pwd, 'gap_analysis.line', 'create', line)
         line_id += 1
 
-def buscar_gap_lines(sock, uid, code):
-    if code == "":
-        attr = []
-    else:
-        attr = code 
-    cod = sock.execute(dbname, uid, pwd, 'gap_analysis.line', 'search', attr)
+def buscar_gap_lines(sock, uid, filt):
+    if filt == "":
+        filt = []
+    cod = sock.execute(dbname, uid, pwd, 'gap_analysis.line', 'search', filt)
     return cod
 
-def listar_gap_lines(sock, uid, code):
+def listar_gap_lines(sock, uid, value):
     lines = []
     fields = []
-    if code == "":
-        attr = []
+    if value == "":
+        filt = []
     else:
-        attr = [('gap_id','=', code)]
-    cod = buscar_gap_lines(sock, uid, attr) 
+        filt = [('gap_id','=', value)]
+    cod = buscar_gap_lines(sock, uid, filt)
     for i in cod:
         line = sock.execute(dbname, uid, pwd, 'gap_analysis.line', 'read', i, fields)
         lines.append(line)
@@ -117,22 +113,20 @@ def act_gap_lines(sock, uid, id_gap):
             values = {'effort' : 2, 'openerp_fct' : 8, 'testing': 2.0}
             mod_gap_lines(sock, uid, l['id'], values)
 
-def buscar_gap_func(sock, uid, code):
-    if code == "":
-        attr = []
-    else:
-        attr = code 
-    cod = sock.execute(dbname, uid, pwd, 'gap_analysis.functionality', 'search', attr)
+def buscar_gap_func(sock, uid, filt):
+    if filt == "":
+        filt = []
+    cod = sock.execute(dbname, uid, pwd, 'gap_analysis.functionality', 'search', filt)
     return cod
 
-def listar_gap_func(sock, uid, code):
+def listar_gap_func(sock, uid, value):
     functionalities = []
     fields = []
-    if code == "":
-        attr = []
+    if value == "":
+        filt = []
     else:
-        attr = [('gap_id','=', code)]
-    cod = buscar_gap_func(sock, uid, attr) 
+        filt = [('gap_id','=', value)]
+    cod = buscar_gap_func(sock, uid, filt)
     for i in cod:
         types = sock.execute(dbname, uid, pwd, 'gap_analysis.functionality', 'read', i, fields)
         functionalities.append(types)
